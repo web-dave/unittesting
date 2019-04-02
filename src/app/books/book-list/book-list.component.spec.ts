09386cffa70a749a4afb1d34fc98c9e6e6a844e7
Dave <dave@webdave.de>
2019-04-02 14:14:38 +0200

    book-list

--- src/app/books/book-list/book-list.component.spec.ts
+++ src/app/books/book-list/book-list.component.spec.ts
@@ -1,35 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { BookListComponent } from './book-list.component';
+import { BookPreviewComponent } from '../shared/mocks/mock.component';
+import { MockBooksService } from '../shared/mocks/mock.book.service';
+import { BookService } from '../shared/book.service';
 
 describe('BookListComponent', () => {
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
+  let view: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookListComponent ]
-    })
-    .compileComponents();
+      declarations: [BookListComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: BookService,
+          useClass: MockBooksService
+        }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    view = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  
+
   it('should have 3 books', () => {
-    expect(false).toBeTruthy();
-  });
-  it('should display the title of each book', () => {
-    expect(false).toBeTruthy();
-  });
-  it('should link to the details page of each book', () => {
-    expect(false).toBeTruthy();
+    expect(component.books.length).toBe(3);
+
+    expect(view.querySelectorAll('app-book-preview').length).toBe(3);
   });
 });
